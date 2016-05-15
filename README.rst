b7477442268fb1b84db51bf8f2308f349cd93837
Lorenz Schori <lo@znerol.ch>
2016-05-15 22:59:09 +0200

    Initial import

---
+++ README.rst
@@ -0,0 +1,15 @@
+SpreadFlow Confviz
+==================
+
+.. image:: https://travis-ci.org/spreadflow/spreadflow-confviz.svg?branch=master
+    :target: https://travis-ci.org/spreadflow/spreadflow-confviz
+
+
+Configuration visualization tool for SpreadFlow metadata extraction and
+processing engine.
+
+
+License
+-------
+
+The software is subject to the MIT license.
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='SpreadFlowConfviz',
+    version='0.0.1',
+    description='Configuration visualization tool for SpreadFlow metadata extraction and processing engine',
+    author='Lorenz Schori',
+    author_email='lo@znerol.ch',
+    url='https://github.com/znerol/spreadflow-confviz',
+    packages=[
+        'spreadflow_confviz',
+    ],
+    entry_points={
+        'console_scripts': [
+            'spreadflow-confviz = spreadflow_confviz:main'
+        ]
+    },
+    install_requires=[
+        'SpreadFlowCore',
+        'graphviz',
+        'toposort',
+    ],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia'
+    ],
+)
