0727da7d541d8cb3b8261a3e5beb020c114a285e
Lorenz Schori <lo@znerol.ch>
2016-06-02 10:02:02 +0200

    Update for changes in core

--- spreadflow_confviz/__init__.py
+++ spreadflow_confviz/__init__.py
@@ -5,7 +5,27 @@ from __future__ import unicode_literals
 
 from spreadflow_core import graph, scheduler
 from spreadflow_core.config import config_eval
-from spreadflow_core.flow import PortCollection
+from spreadflow_core.component import PortCollection
+from spreadflow_core.dsl import \
+    AddTokenOp, \
+    AliasResolverPass, \
+    ComponentToken, \
+    ComponentsPurgePass, \
+    ConnectionToken, \
+    DescriptionToken, \
+    EventHandlerToken, \
+    EventHandlersPass, \
+    LabelToken, \
+    PartitionBoundsPass, \
+    PartitionControllersPass, \
+    PartitionExpanderPass, \
+    PartitionSelectToken, \
+    PartitionWorkerPass, \
+    PortsValidatorPass, \
+    portmap
+from spreadflow_core.dsl.compiler import \
+    token_attr_map, \
+    stream_extract
 from graphviz import Digraph
 from pprint import pformat
 from toposort import toposort
@@ -18,6 +38,8 @@ class ConfvizCommand(object):
 
     path = None
     level = 1
+    multiprocess = False
+    partition = None
 
     def __init__(self, out=sys.stdout):
         self._out = out
@@ -33,18 +55,54 @@ class ConfvizCommand(object):
                             help='Path to config file')
         parser.add_argument('-l', '--level', type=int,
                             help='Level of detail (0: toplevel components, default: 1)')
+        parser.add_argument('-p', '--multiprocess', action='store_true',
+                            help='Simulates multiprocess support, i.e., launch a separate process for each chain')
+        parser.add_argument('--partition',
+                            help='Simulates multiprocess support, select the given partition of the graph')
 
         parser.parse_args(args[1:], namespace=self)
 
         is_port_collection = lambda c: isinstance(c, PortCollection)
 
-        flowmap = config_eval(self.path)
-        links = flowmap.compile()
-        outs, ins = zip(*links)
+        stream = config_eval(self.path)
+
+        pipeline = list()
+        pipeline.append(AliasResolverPass())
+        pipeline.append(PortsValidatorPass())
+
+        if self.multiprocess:
+            pipeline.append(PartitionExpanderPass())
+            pipeline.append(PartitionBoundsPass())
+            if self.partition:
+                pipeline.append(PartitionWorkerPass())
+                partition = self.partition
+                stream.append(AddTokenOp(PartitionSelectToken(partition)))
+            else:
+                pipeline.append(PartitionControllersPass())
+
+        pipeline.append(ComponentsPurgePass())
+        pipeline.append(EventHandlersPass())
+
+        for compiler_step in pipeline:
+            stream = compiler_step(stream)
+
+        connection_ops, stream = stream_extract(stream, ConnectionToken)
+        connections = list(portmap(connection_ops).items())
+
+        component_ops, stream = stream_extract(stream, ComponentToken)
+        components = list(token_attr_map(component_ops, 'element'))
+
+        label_ops, stream = stream_extract(stream, LabelToken)
+        labels = token_attr_map(label_ops, 'element', 'label')
+
+        description_ops, stream = stream_extract(stream, DescriptionToken)
+        descriptions = token_attr_map(description_ops, 'element', 'description')
+
+        outs, ins = zip(*connections)
 
         # Build up parent-child relationships.
         comp_tree = []
-        for comp in set(list(ins) + list(outs) + list(flowmap.annotations.keys())):
+        for comp in set(list(ins) + list(outs) + list(components)):
             if is_port_collection(comp):
                 for subcomp in set(comp.ins + comp.outs):
                     if comp is not subcomp:
@@ -56,35 +114,37 @@ class ConfvizCommand(object):
         parents = graph.reverse(children)
         levels = list(toposort(parents))
 
-        self.level = min(self.level, len(levels) - 1)
+        self.level = min(self.level, len(levels))
 
         # Pretend that components at the specified level are fully connected.
         extra_links = []
-        for comp in levels[self.level]:
-            if is_port_collection(comp):
-                for port_in in comp.ins:
-                    for port_out in comp.outs:
-                        if port_in is not port_out:
-                            extra_links.append((port_in, port_out))
+        if self.level < len(levels):
+            for comp in levels[self.level]:
+                if is_port_collection(comp):
+                    for port_in in comp.ins:
+                        for port_out in comp.outs:
+                            if port_in is not port_out:
+                                extra_links.append((port_in, port_out))
 
         # Build up the flow graph.
-        g = graph.digraph(links + extra_links)
+        g = graph.digraph(connections + extra_links)
         rg = graph.reverse(g)
 
         # Only show ports at the specified level if they have a connection to
         # ports with another parent.
         ignorable_comps = set()
-        for comp in levels[self.level]:
-            if not is_port_collection(comp):
-                my_parents = parents.get(comp, [])
-                conns = g.get(comp, set()).union(rg.get(comp, set()))
-                for peer in conns:
-                    if my_parents != parents.get(peer):
-                        break
-                else:
-                    # Ignore this port if it has no peers or all of them a have the
-                    # same parent.
-                    ignorable_comps.add(comp)
+        if self.level < len(levels):
+            for comp in levels[self.level]:
+                if not is_port_collection(comp):
+                    my_parents = parents.get(comp, [])
+                    conns = g.get(comp, set()).union(rg.get(comp, set()))
+                    for peer in conns:
+                        if my_parents != parents.get(peer):
+                            break
+                    else:
+                        # Ignore this port if it has no peers or all of them a have the
+                        # same parent.
+                        ignorable_comps.add(comp)
 
         # Ignore everything which is beyond the specified level.
         for comps in levels[self.level+1:]:
@@ -98,15 +158,20 @@ class ConfvizCommand(object):
 
         # Build clusters wrapping port collections.
         subgraphs = {}
+        visible_ports = set(ins + outs)
         for level in reversed(levels[:self.level]):
             for c in level:
                 if is_port_collection(c):
+                    ports = set(c.ins + c.outs).intersection(visible_ports)
+                    if len(ports) == 0:
+                        continue
+
                     sg = Digraph('cluster_{:s}'.format(str(hash(c))))
-                    label = flowmap.annotations[c].get('label', self._strip_angle_brackets(str(c)))
-                    tooltip = flowmap.annotations[c].get('description', repr(c) + "\n" + pformat(vars(c)))
+                    label = labels.get(c, self._strip_angle_brackets(str(c)))
+                    tooltip = descriptions.get(c, repr(c) + "\n" + pformat(vars(c)))
                     sg.attr('graph', label=label, tooltip=tooltip, color="blue")
 
-                    for port in set(c.ins + c.outs):
+                    for port in ports:
                         if port not in ignorable_comps:
                             sg.node(str(hash(port)))
                         if port in subgraphs:
@@ -125,10 +190,10 @@ class ConfvizCommand(object):
         # Tooltips
         for n in graph.vertices(g):
             try:
-                tooltip = flowmap.annotations[n].get('description', repr(n) + "\n" + pformat(vars(n)))
+                tooltip = descriptions.get(n, repr(n) + "\n" + pformat(vars(n)))
             except TypeError:
                 tooltip = ''
-            label = flowmap.annotations[n].get('label', self._strip_angle_brackets(str(n)))
+            label = labels.get(n, self._strip_angle_brackets(str(n)))
             dg.node(str(hash(n)), label=label, tooltip=tooltip)
 
         print(dg.pipe(format='svg'), file=self._out)
